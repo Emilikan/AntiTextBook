02491232ac75d3cdd8ec7327fd096f47d44e1106
dmitriy0 <sevdmitriy0804@mail.ru>
2018-12-08 20:52:22 +0300

    add choosePDF

--- app/src/main/java/com/example/antitextbook/Storage.java
+++ app/src/main/java/com/example/antitextbook/Storage.java
@@ -1,30 +1,43 @@
 package com.example.antitextbook;
 
+import android.annotation.SuppressLint;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.preference.PreferenceManager;
+import android.provider.MediaStore;
 import android.support.annotation.RequiresApi;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Objects;
 
+import static android.app.Activity.RESULT_OK;
+
 public class Storage extends Fragment {
     private ListView listBooks;
+    private static final int PICK_PDF_REQUEST = 345;
     private ArrayList<String> mBooks = new ArrayList<String>();
     private ArrayList<String> arrPdfUri = new ArrayList<String>();
     private String pdfUri;
+    private Uri filePdfPath = null;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -68,7 +81,41 @@ public class Storage extends Fragment {
         }
 
         updateUI();
+        Button choosePdf = rootView.findViewById(R.id.buttonChooseBook);//Кнопка загрузки pdf
+        choosePdf.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pdfChooser();
+
+                //Toast.makeText(getContext(), pdfUri, Toast.LENGTH_LONG).show();
+
+            }
+        });
+
         return rootView;
+    }
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == PICK_PDF_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null){
+            filePdfPath = data.getData();
+            Toast.makeText(getContext(),filePdfPath + "",Toast.LENGTH_SHORT).show();
+            SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getContext());
+            SharedPreferences.Editor editor = preferences.edit();
+            editor.putString("URI", String.valueOf(filePdfPath));
+            editor.apply();
+
+            Fragment fragment = new Home();
+            FragmentManager fragmentManager = Objects.requireNonNull(getActivity()).getSupportFragmentManager();
+            fragmentManager.beginTransaction().replace(R.id.container, fragment).commit();
+        }
+    }
+    private void pdfChooser() {
+        Intent intent = new Intent();
+        intent.setType("application/pdf");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select PDF"), PICK_PDF_REQUEST);
     }
     public void updateUI() {
         if (getActivity() != null) {
